146656bb02849b085aa6554a85c9c8ee858b8acc
JakeDEsposito <97902257+JakeDEsposito@users.noreply.github.com>
2022-10-18 17:13:14 -0400

    Added comments

--- res/game.js
+++ res/game.js
@@ -1,19 +1,28 @@
-/*
-player and opponent are both hex numbers
-0x001 : rock
-0x010 : paper
-0x100 : scissors
+// game.js Created by Jake D'Esposito in 2022
+// This isn't lisensed but if you use code from here it would be nice if you credit me or like to my Github or website.
 
-returns -1 if the opponent won
-returns 1 if the player won
-returns 0 if there is a tie
+/**
+ * @param {Number} player is a hex numbers that can be 0x001, 0x010, or 0x100
+ * @param {Number} opponent is a hex numbers that can be 0x001, 0x010, or 0x100
+ * 
+ * 0x001 : rock
+ * 0x010 : paper
+ * 0x100 : scissors
+ *
+ * @returns {-1} if the opponent won
+ * @returns {1} if the player won
+ * @returns {0} if there is a tie
 */
 function determineGame (player, opponent) {
+    // If the player and opponent pick the same option, the there is a tie
     if (player === opponent)
         return 0
     else {
+        // A combination of player & opponent
         const combinedHex = player | opponent
         
+        // Checks which of the possible 3 combinations have been inputed
+        // When a case that matches is found, it if the player picked the winning option and returns a winner appropriatly
         switch (combinedHex) {
             case 0x110:
                 if (player === 0x100)
@@ -31,25 +40,41 @@ function determineGame (player, opponent) {
     }
 }
 
+// Initializes sound effect to be used later on
 const winSound = new Audio("res/win.wav")
 const tieSound = new Audio("res/tie.wav")
 const lossSound = new Audio("res/loss.wav")
+const rewindSound = new Audio("res/rewind.wav")
 
+/**
+ * This function is called when clicking on the rock, paper, or scissors
+ * @param {Number} playerChoice the hex value that represents the choice. See determineGame.
+ * 
+ * @return {void}
+ */
 function playGame (playerChoice) {
 
     // Make opponent choice
     let shift = Math.floor(Math.random() * 3)
+    // It is possible that shift is 3 but there isn't 4 options to map 3 to 2.
     if (shift === 3)
         shift = 2
 
+    // A representation of the computers choice in hex.
     const opponentChoice = 0x1 << (shift * 4)
 
     const gameResult = determineGame(playerChoice, opponentChoice)
 
+    // Removes the icon from Opponents Choice section
     $("#opponent-choice-icon").text("")
+
     $("#player-choice-text").removeClass("pulse")
     $("#opponent-choice-text").addClass("pulse")
+
+    // Plays animation for spinner then does something else
     animateOpponentSpinner().then(() => {
+
+        // Analyzes game result and takes approprate action
         switch (gameResult) {
             case 1:
                 winSound.play()
@@ -72,26 +97,46 @@ function playGame (playerChoice) {
     })
 }
 
+const storage = localStorage
+
+/**
+ * Safely gets the score for wins, ties, or losses as a number.
+ * @param {"wins" | "ties" | "losses"} type 
+ * 
+ * If it exists, it will return its value.
+ * If it doesn't exist, it will set its value to 0 and return 0.
+ * 
+ * @returns {Number}
+ */
 function getScore (type) {
-    const localStorageScore = localStorage.getItem(type)
-    if (localStorageScore)
-        return new Number(localStorageScore)
+    const storageScore = storage.getItem(type)
+    if (storageScore)
+        return new Number(storageScore)
     else {
-        localStorage.setItem(type, 0)
+        storage.setItem(type, 0)
         return 0
     }
 }
 
+/**
+ * Increments the score for wins, ties, or losses.
+ * @param {"wins" | "ties" | "losses"} type 
+ * 
+ * @returns {Number}
+ */
 function incrementScore (type) {
     const score = getScore(type)
-    localStorage.setItem(type, score + 1)
+    storage.setItem(type, score + 1)
 }
 
-const rewindSound = new Audio("res/rewind.wav")
-
+/**
+ * Clears storage of any scores
+ * @param {void}
+ * @return {void}
+ */
 function clearScores () {
     rewindSound.play()
-    localStorage.clear()
+    storage.clear()
     update()
 }
 
@@ -139,7 +184,16 @@ function gameResultText (gameResult) {
     }
 }
 
+/**
+ * Updates the wins, ties, and looses on the webpage
+ * @param {Number} playerChoice is a hex numbers that can be 0x001, 0x010, or 0x100. See determineGame.
+ * @param {Number} opponentChoice is a hex numbers that can be 0x001, 0x010, or 0x100. See determineGame.
+ * @param {Number} gameResult is a hex numbers that can be 0x001, 0x010, or 0x100. See determineGame.
+ * 
+ * @return {void}
+ */
 function update (playerChoice, opponentChoice, gameResult) {
+    // Updates the wins, ties, and looses text
     $("#wins").text(getScore("wins"))
     $("#ties").text(getScore("ties"))
     $("#losses").text(getScore("losses"))
@@ -148,11 +202,18 @@ function update (playerChoice, opponentChoice, gameResult) {
     $("#ties").removeClass("pulse-once")
     $("#losses").removeClass("pulse-once")
 
+    // On the initial page load, gameResult will be undefined so we can skip all of this. Otherwise run it.
     if (gameResult !== undefined) {
+
+        // Sets icon of then opponents choice
         $("#opponent-choice-icon").text(choiceToIcon(opponentChoice))
+
         $("#gameResult").css("visibility", "visible")
+
+        // Updates the game result text
         $("#gameResult").text(`${choiceToString(playerChoice)} ${gameResultVsText(gameResult)} ${choiceToString(opponentChoice).toLowerCase()}. You ${gameResultText(gameResult)}!`)
 
+        // Pulses the score that gets changed based on gameResult
         switch (gameResult) {
             case 1:
                 $("#wins").addClass("pulse-once")
@@ -167,25 +228,38 @@ function update (playerChoice, opponentChoice, gameResult) {
     }
 }
 
+// Calls update on file load
 update()
 
+// Fun promise based delay that I found on stack overflow lol. Thanks mystery internet person.
 const delay = async (ms = 1000) => new Promise(resolve => setTimeout(resolve, ms))
 
+/**
+ * Animates the opponent choice spinner
+ * @param {void}
+ * @return {void}
+ */
 async function animateOpponentSpinner () {
 
+    // Makes spinner render
     $("#opponent-spinner").css("display", "revert")
 
     await delay(2000)
 
+    // Slows spinning speed
     $("#opponent-spinner").attr("scrollamount", 25)
 
     await delay(2000)
 
+    // Slows spinning speed again
     $("#opponent-spinner").attr("scrollamount", 10)
 
     await delay(2000)
 
+    // Makes spinner not render
     $("#opponent-spinner").css("display", "none")
+
+    // Resets spinning speed for the next time that it has to get rendered
     $("#opponent-spinner").attr("scrollamount", 50)
 
 }
